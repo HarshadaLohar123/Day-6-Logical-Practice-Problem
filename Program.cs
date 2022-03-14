5099838b59b0b6e025d87a59190a01de0a55af14
harshadalohar123 <harshalohar123@gmail.com>
2022-03-14 15:48:23 +0530

    ADDED:Reverse a Number

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ReverseNo
+{
+    internal class ReverseNumber
+    {
+        static void Main(string[] args)
+        {
+            int n;
+
+            Console.WriteLine("Enter Any Number :");
+            n = int.Parse(Console.ReadLine());
+
+            //Repeat the loop till n greater than 0
+            while (n > 0)
+            {
+                //Get the Last digit of Number by MOD
+                int r = n % 10;
+                Console.WriteLine(r);
+                //Delete the Last Digit
+                n = n / 10;
+            }
+
+        }
+    }
+}
